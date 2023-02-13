a7d832bbd738c2688d457d98881b575653d7444a
Saf <120998128+safran2k@users.noreply.github.com>
2023-02-13 00:02:57 +0000

    create initial phase of working etch-a-sketch
    create index.html, javascript.js, style.css
    add working grid with "choose grid size" and "erase" button, as well as options to pick from 9 different colours.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # etch-a-sketch
-etch-a-sketch project, containing a 16x16 grid of pixels that change colour when hovered over
+Etch-a-sketch project, containing a resizeable grid of pixels that change colour when hovered over.
+User is able to select the colour they want and erase the grid if necessary.
+
+
+[**Live preview**](https://safran2k.github.io/etch-a-sketch/index.html)
---
+++ javascript.js
@@ -0,0 +1,76 @@
+// let gridsize = 16;
+const gridContainer = document.querySelector('#grid-container');
+const gridSizeButton = document.querySelector('#grid-size-button');
+const eraseButton = document.querySelector('#erase-button');
+let currentColor = "blue";
+let rows;
+generateGrid(16);
+rows = document.querySelectorAll('.row');
+colors = document.querySelectorAll('.color-picker');
+
+function setColor(divContent) {
+    divContent.style.backgroundColor = currentColor;
+}
+
+function generateGrid(gridsize) {
+    for(let y =0; y<gridsize; y++) {
+        const row = document.createElement('div');
+        gridContainer.appendChild(row);
+        row.classList.add('row');
+        for(let x=0; x<gridsize; x++) {
+            const pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            const pixelsize = 800/gridsize;
+            row.style.height = pixelsize + "px";
+            // took me a while to figure out i had to do this
+            pixel.addEventListener('mouseenter', () =>{
+                setColor(pixel);
+            });
+    
+    
+            // pixel.style.height = pixelsize + "px";
+            // pixel.style.width = pixelsize + "px";
+            row.appendChild(pixel);
+    
+        }
+    }
+
+    rows = document.querySelectorAll('.row');
+}
+
+function deleteGrid(gridRows) {
+    gridRows.forEach(currentRow => {
+        gridContainer.removeChild(currentRow);
+    });
+}
+
+gridSizeButton.addEventListener('click', () => {
+    gridsize = + prompt("Enter the number of pixels wide you would like your grid to be (up to 100). Entering 30 will give you a 30x30 size grid")
+    if((gridsize < 101) && (gridsize > 0)){
+        deleteGrid(rows);
+        generateGrid(gridsize);
+    } else {
+        alert("You must enter a number between 1 and 100");
+    }
+
+});
+
+eraseButton.addEventListener('click', () => {
+    const pixels = document.querySelectorAll('.pixel');
+    pixels.forEach(currentPixel => {
+        currentPixel.style.backgroundColor = "white";
+    })
+});
+
+colors.forEach(selectedColor => selectedColor.addEventListener('click', () => {
+    const colorStyle = getComputedStyle(selectedColor);
+    currentColor = colorStyle.backgroundColor;
+    // console.log(selectedColor.id);
+}));
+
+
+
+
+
+
+// gridContainer.classList.add('pixel');
